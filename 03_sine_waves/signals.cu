73859f2dfbb8fd923b8e898716aed3c210dd6055
Rob Armstrong <roarmstrong@nvidia.com>
2024-09-23 13:10:58 -0700

    03: Add FFT with magnitude calculation, visualization script

--- 03_sine_waves/signals.cu
+++ 03_sine_waves/signals.cu
@@ -2,9 +2,16 @@
 #include <fstream>
 #include <thrust/device_vector.h>
 #include <thrust/tabulate.h>
+#include <cufft.h>
 #include <array>
+#include <nvtx3/nvToolsExt.h>
 
 #define NUM_SAMPLES (1024 * 1024)
+
+#define FREQ_A (440.0f)
+#define FREQ_E (659.255f)
+
+#define SAMPLE_RATE (44100.0f)
 
 struct sine_wave_functor
 {
@@ -25,30 +32,74 @@ struct sine_wave_functor
 int main(void)
 {
     // Quick call to cudaFree to ensure context creation
+    nvtxRangePushA("Context creation");
     cudaFree(0);
+    nvtxRangePop();
 
     // Create a device vector to hold out input waveform
+    nvtxRangePushA("Memory Initialization");
     thrust::device_vector<float> d_vec(NUM_SAMPLES);
     thrust::device_vector<float> d_vec2(NUM_SAMPLES);
     thrust::device_vector<float> d_combined(NUM_SAMPLES);
 
     // Initialize the two device vectors
+    nvtxRangePushA("Populate waves");
     thrust::tabulate(
         d_vec.begin(),
         d_vec.end(),
-        sine_wave_functor(1, 2 * M_PI / NUM_SAMPLES, 0));
+        sine_wave_functor(1, 2 * M_PI * FREQ_A / SAMPLE_RATE, 0));
     thrust::tabulate(
         d_vec2.begin(),
         d_vec2.end(),
-        sine_wave_functor(0.5, 4 * M_PI / NUM_SAMPLES, 0));
+        sine_wave_functor(0.5, 2 * M_PI * FREQ_E / SAMPLE_RATE, 0));
+    nvtxRangePop();
 
     // Add all of the component waves
+    nvtxRangePushA("Add waves");
     thrust::transform(d_vec.begin(), d_vec.end(), d_vec2.begin(), d_combined.begin(), thrust::plus<float>());
+    nvtxRangePop();
 
+    nvtxRangePop();
+
+    // Now let's do an FFT on the combined waveform
+    nvtxRangePushA("FFT");
+    cufftHandle plan;
+    cufftResult result;
+
+    int complex_size = NUM_SAMPLES / 2 + 1;
+    thrust::device_vector<cufftComplex> d_fft(complex_size);
+
+    result = cufftPlan1d(&plan, NUM_SAMPLES, CUFFT_R2C, 1);
+    if (result != CUFFT_SUCCESS)
+    {
+        std::cout << "CUFFT Error: Plan creation failed" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    result = cufftExecR2C(plan, d_combined.data().get(), d_fft.data().get());
+    if (result != CUFFT_SUCCESS)
+    {
+        std::cout << "CUFFT Error: ExecR2C failed" << std::endl;
+        return EXIT_FAILURE;
+    }
+    nvtxRangePop();
+
+    // On-device magnitude spectrum
+    nvtxRangePushA("FFT bin magnitudes");
+    auto mag = thrust::transform_iterator(d_fft.begin(), [] __host__ __device__(cufftComplex c)
+                                          { return sqrtf(c.x * c.x + c.y * c.y) / NUM_SAMPLES; });
+    thrust::device_vector<float> d_mags{mag, mag + d_fft.size()};
+    nvtxRangePop();
+
+    nvtxRangePushA("Copy results");
     std::vector<float> h_combined(d_combined.size());
+    std::vector<float> h_mags(d_mags.size());
 
     thrust::copy(d_combined.begin(), d_combined.end(), h_combined.begin());
+    thrust::copy(d_mags.begin(), d_mags.end(), h_mags.begin());
+    nvtxRangePop();
 
+    nvtxRangePushA("Write results");
     std::ofstream outfile("output.csv");
     if (outfile.is_open())
     {
@@ -62,6 +113,21 @@ int main(void)
     {
         std::cout << "Unable to open file" << std::endl;
     }
+
+    std::ofstream of2("mags.csv");
+    if (of2.is_open())
+    {
+        for (auto &val : h_mags)
+        {
+            of2 << val << "\n";
+        }
+        of2.close();
+    }
+    else
+    {
+        std::cout << "Unable to open file" << std::endl;
+    }
+    nvtxRangePop();
 
     return EXIT_SUCCESS;
 }
